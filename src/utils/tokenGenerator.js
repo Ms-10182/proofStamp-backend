864febbb4716169470ae520d30feff8e8852c6bb
Mayank Sharma <22bcs10182@cuchd.in>
2025-09-02 17:16:42 +0530

    feat: add validation for user fields in token generation

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -1,7 +1,11 @@
 import jsonwebtoken from "jsonwebtoken";
+import { ApiError } from "./ApiError.js";
 
 const generateToken = (user) => {
     const {address,contractAddress} = user;
+    if(!address || !contractAddress){
+        throw new ApiError(400,"all fields are required")
+    }
     const token = jsonwebtoken.sign({ address, contractAddress }, process.env.TOKEN_SECRET, {
         expiresIn: process.env.TOKEN_EXPIRY
     });
